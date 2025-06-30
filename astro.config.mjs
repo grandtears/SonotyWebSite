e2cfa7c1b9cab955ae6166eaa2b0ad4fb7d95328
grandtears <grandtears.sonoty@gmail.com>
2025-06-30 12:24:21 +0900

    SonotyWebSite first Commit.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+// 1. react をインポートします
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // 2. integrations 配列に react() を追加します
+  integrations: [react()],
+
+  // 以前設定したDev Toolbarを無効にする設定
+  devToolbar: {
+    enabled: false,
+  },
+});
